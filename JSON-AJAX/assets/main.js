b242c2ff44e12136daf8855daf322018657e247b
karleighponnock <karleighponnock@gmail.com>
2020-06-19 22:03:24 -0400

    making a request

--- JSON-AJAX/assets/main.js
+++ JSON-AJAX/assets/main.js
@@ -1,40 +1,10 @@
-
-// Object
-
-var myCat = {
-    //property:value
-
-    "name": "Meowsalot",
-    "species": "cat",
-    "favFood": "tuna"
-}
-
-//access properties values from object and console log it
-console.log(myCat.favFood);
-
-
-//Array- comma seperated list of values
-var myFavColors = ["blue", "green", "purple", "red"];
-
-//access properties values from array and console log it
-
-console.log(myFavColors[2]);
-
-
-//COMBINE
-//array
-var thePets = [
-//Nested object
-    { "name": "Meowsalot",
-    "species": "cat",
-    "favFood": "tuna"
-},
-//Nested object
-    { "name": "Barky",
-    "species": "dog",
-    "favFood": "carrots"}
-];
-//access properties values and console log it
-//access object using index then .whateverProperty of the value you want to access
-//console log to check- should print carrots
-console.log(thePets[1].favFood);
+//new instance for this tool
+var ourRequest = new XMLHttpRequest();
+//define request
+//get request from the url
+ourRequest.open(`GET`, `https://learnwebcode.github.io/json-e...`);
+//when it loads run function
+ourRequest.onload = function(){
+    //text response info
+console.log(ourRequest.responseText)
+};
