fdb54ddd3dd74174b209ae7aa1dd3be5ca33849e
karleighponnock <karleighponnock@gmail.com>
2020-06-19 22:23:17 -0400

    request is made info is parsed back out to console currently

--- JSON-AJAX/assets/main.js
+++ JSON-AJAX/assets/main.js
@@ -2,9 +2,13 @@
 var ourRequest = new XMLHttpRequest();
 //define request
 //get request from the url
-ourRequest.open(`GET`, `https://learnwebcode.github.io/json-e...`);
+ourRequest.open(`GET`, `https://learnwebcode.github.io/json-example/animals-1.json`);
 //when it loads run function
 ourRequest.onload = function(){
-    //text response info
-console.log(ourRequest.responseText)
+    //parse information so it doesnt interpret it as a string
+    //access objects with index instead of individual characters
+    var ourData = JSON.parse(ourRequest.responseText);
+    console.log(ourData[0]);
 };
+
+ourRequest.send();
