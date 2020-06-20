cacad350f87539fb4913191696434b3934287442
karleighponnock <karleighponnock@gmail.com>
2020-06-19 22:34:18 -0400

    rendering data to html

--- JSON-AJAX/assets/main.js
+++ JSON-AJAX/assets/main.js
@@ -1,14 +1,31 @@
-//new instance for this tool
-var ourRequest = new XMLHttpRequest();
-//define request
-//get request from the url
-ourRequest.open(`GET`, `https://learnwebcode.github.io/json-example/animals-1.json`);
-//when it loads run function
-ourRequest.onload = function(){
-    //parse information so it doesnt interpret it as a string
-    //access objects with index instead of individual characters
-    var ourData = JSON.parse(ourRequest.responseText);
-    console.log(ourData[0]);
-};
 
-ourRequest.send();
+var animalContainer = document.getElementById("animal-info");
+var btn = document.getElementById("btn");
+
+
+btn.addEventListener("click", function () {
+
+    //new instance for this tool
+    var ourRequest = new XMLHttpRequest();
+    //define request
+    //get request from the url
+    ourRequest.open(`GET`, `https://learnwebcode.github.io/json-example/animals-1.json`);
+    //when it loads run function
+    ourRequest.onload = function () {
+        //parse information so it doesnt interpret it as a string
+        //access objects with index instead of individual characters
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+    };
+    ourRequest.send();
+})
+
+
+function renderHTML(data) {
+    var htmlString = "this is a test";
+animalContainer.insertAdjacentHTML(`beforeend`, htmlString);
+}
+
+
+
+
