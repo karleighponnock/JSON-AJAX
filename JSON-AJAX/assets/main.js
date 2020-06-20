3544eaed584c1d84dbf481c0c5dc46d99c37b7d3
karleighponnock <karleighponnock@gmail.com>
2020-06-19 21:27:04 -0400

    ARRAY

--- JSON-AJAX/assets/main.js
+++ JSON-AJAX/assets/main.js
@@ -1,7 +1,21 @@
-var myCat= {
+
+// Object
+
+var myCat = {
+    //property:vlaue
+
     "name": "Meowsalot",
     "species": "cat",
     "favFood": "tuna"
 }
 
+//access properties values from object and console log it
 console.log(myCat.favFood);
+
+
+//Array- comma seperated list of values
+var myFavColors = ["blue", "green", "purple", "red"];
+
+//access properties values from array and console log it
+
+console.log(myFavColors[2]);
